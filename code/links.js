3c1e2fe95c1cff3f675ae476ab66e38fc376f174
KazimKeskin <115115454+KazimKeskin@users.noreply.github.com>
2025-01-18 09:50:04 +0300

    updated findFileFromLink to use decodeURI for href

--- code/links.js
+++ code/links.js
@@ -41,7 +41,7 @@ async function validateAsset(link) {
 
 function findFileFromLink(href, jsonData) {
   for (const key in jsonData) {
-    if (jsonData[key].filepath == href) {
+    if (jsonData[key].filepath === decodeURI(href)) {
       return jsonData[key].filepath;
     }
   }
